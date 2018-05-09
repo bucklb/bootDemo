7bd2dc155c6c648cac89a2ac2c5258999440c887
bob buckley <bob.buckley@dwp.gsi.gov.uk>
2018-05-09 19:07:52 +0100

    added a commandLineRunner to mix

---
+++ src/main/java/udemy/spring/bootdemo/BootRunner.java
@@ -0,0 +1,15 @@
+package udemy.spring.bootdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(1)   // in case we add other runners, need to state an order to run them
+public class BootRunner implements CommandLineRunner{
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println("Runner is running ...");
+    }
+}
